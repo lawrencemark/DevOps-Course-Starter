05c81534c134bb675287021258483b5f2f87a2e8
lawrencemark <marklawrence@hotmail.co.uk>
2021-03-29 12:10:16 +0100

    Add files via upload
    Updated with hopefully the right type of changes

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from todo_app.data.session_items import *
 from todo_app.flask_config import Config
 
@@ -6,14 +6,18 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/', methods = ['POST', 'GET'])
-def index():
+@app.route('/', methods = ['POST'])
+def postRequest():
     if request.method == 'POST':
         itemAdded = request.form['addTo']
         add_item(itemAdded)
-        return render_template('index.html', lstToDo = get_items())
+        return redirect(url_for('index'))
     else:
-        return render_template('index.html', lstToDo = get_items()) 
+        return "Something went wrong!"
+
+@app.route('/', methods = ['GET'])
+def index():
+    return render_template('index.html', lstToDo = get_items()) 
 
 
 if __name__ == '__main__':
