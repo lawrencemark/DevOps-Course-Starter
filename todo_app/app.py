adade081e36c2ecbcb9aaacc31994acf5d53baf0
lawrencemark <marklawrence@hotmail.co.uk>
2021-03-29 11:19:47 +0100

    updated

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,19 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from todo_app.data.session_items import *
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return 'Hello World!'
+    if request.method == 'POST':
+        itemAdded = request.form['addTo']
+        add_item(itemAdded)
+        return render_template('index.html', lstToDo = get_items())
+    else:
+        return render_template('index.html', lstToDo = get_items()) 
 
 
 if __name__ == '__main__':
